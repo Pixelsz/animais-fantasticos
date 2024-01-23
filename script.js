2adfe06f957333dc229a06cb3f5ffcb207347823
Pixelsz <pinkdarin@gmail.com>
2024-01-23 11:01:04 -0300

    added soft scroll and accordion list

--- script.js
+++ script.js
@@ -39,3 +39,48 @@ accordionList.forEach((item) => {
 }
 }
 initAccordion();
+
+function initScrollSuave() {
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+console.log(linksInternos)
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+    })
+}
+
+linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+})
+}
+
+initScrollSuave();
+
+const sections = document.querySelectorAll('.js-scroll');
+
+function initAnimacaoScroll () {
+if(sections.length) {
+const windowMetade = window.innerHeight * 0.6;
+
+function animaScroll() {
+    sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if(isSectionVisible) {
+            section.classList.add('ativo');
+        }
+    })
+}
+
+animaScroll();
+
+window.addEventListener('scroll', animaScroll);
+}
+}
+
+initAnimacaoScroll();
