e7dcb8b841057a0bbae6ce0593e6ebac3560e059
Pixelsz <pinkdarin@gmail.com>
2024-01-23 01:54:35 -0300

    fixed smooth slideDown transition

--- script.js
+++ script.js
@@ -21,13 +21,21 @@ tabMenu.forEach((itemMenu, index) => {
 };
 initTabNav();
 
+function initAccordion() {
 const accordionList = document.querySelectorAll('.js-accordion dt');
+const activeClass = 'ativo';
+if(accordionList.length) {
+accordionList[0].classList.add(activeClass);
+accordionList[0].nextElementSibling.classList.add(activeClass);
 
 function activeAccordion() {
-    this.classList.toggle('ativo');
-    this.nextElementSibling.classList.toggle('ativo');
+    this.classList.toggle(activeClass);
+    this.nextElementSibling.classList.toggle(activeClass);
 }
 
 accordionList.forEach((item) => {
     item.addEventListener('click', activeAccordion);
-})
+});
+}
+}
+initAccordion();
