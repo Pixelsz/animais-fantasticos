43e46aa28db974eaf30a64afcac80a32194040db
Pixelsz <pinkdarin@gmail.com>
2024-01-30 12:14:44 -0300

    fixed scroll height to fit all screen sizes

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const sections = document.querySelectorAll('.js-scroll');
 
 function initAnimacaoScroll () {
 if(sections.length) {
-const windowMetade = window.innerHeight * 0.6;
+const windowMetade = window.innerHeight * 0.8;
 
 function animaScroll() {
     sections.forEach((section) => {
